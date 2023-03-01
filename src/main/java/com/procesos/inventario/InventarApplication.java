dd154f23215850c015aca0bae1a29f08949dac6b
Sergio Garcia <slgarciar@ufpso.edu.co>
2023-02-28 21:32:34 -0500

    creacion del proyecto de clase

---
+++ src/main/java/com/procesos/inventario/InventarApplication.java
@@ -0,0 +1,13 @@
+package com.procesos.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/procesos/inventario/InventarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procesos.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
