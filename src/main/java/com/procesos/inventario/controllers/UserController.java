a5e2313e9abff9229021b016ac5e81a50a258fa0
Sergio Garcia <Slgarciar@ufpso.edu.co>
2023-03-14 21:03:33 -0500

    agregar servicios y endpoin de crear usuario

--- src/main/java/com/procesos/inventario/controllers/UserController.java
+++ src/main/java/com/procesos/inventario/controllers/UserController.java
@@ -1,20 +1,23 @@
 package com.procesos.inventario.controllers;
 
 import com.procesos.inventario.models.User;
-import com.procesos.inventario.services.UserService;
+import com.procesos.inventario.services.UserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RestController
 public class UserController {
     @Autowired
-    private UserService UserService;
+    private UserServiceImp UserServiceImp;
 
-    @GetMapping(value = "user/{id}")
-    public Optional findUserById(Long id){
-        return UserService.getUser(id);
+    @GetMapping(value = "/user/{id}")
+    public User findUserById(@PathVariable Long id){
+        return UserServiceImp.getUser(id);
+    }
+    @PostMapping(value = "user")
+    public Boolean saveUser(@RequestBody User user){
+        return userServiceImp.createUser(user);
     }
 }
--- src/main/java/com/procesos/inventario/models/User.java
+++ src/main/java/com/procesos/inventario/models/User.java
@@ -2,9 +2,7 @@ package com.procesos.inventario.models;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
@@ -12,14 +10,14 @@ import java.util.Date;
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name ="first_name")
     private String firstName;
     @Column(name ="last_name")
     private String lastName;
-    private String Address;
-    private String Email;
+    private String address;
+    private String email;
     private String password ;
     private Date birthday;
-
 }
